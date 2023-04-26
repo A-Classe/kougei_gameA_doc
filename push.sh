e0f520c6883f8b8bb74111d6071cba6a1c7a2521
Matuyuhi <bird9.yuhi@gmail.com>
2023-04-26 14:23:02 +0900

    replace message to en

--- push.sh
+++ push.sh
@@ -12,24 +12,25 @@ git status --short
 current_branch=$(git symbolic-ref --short HEAD)
 
 # ブランチを切り替えるか、新しいブランチを作成するかをユーザーに尋ねる
-read -p "now branch is [${current_branch}]  create new work branch？ (y/n): " is_create_branch
+read -p "Now branch is [${current_branch}]  create new work branch？ (y/n): " is_create_branch
 if [ "$is_create_branch" = "y" ] || [ "$is_create_branch" = "Y" ]; then
   # ブランチの一覧を表示する
   branches=$(git branch --list)
-  read -p "changed found branch? (y/n)" switch_branch 
+  # 既存のブランチに切り替えるか？
+  read -p "Switch to an existing branch? (y/n)" switch_branch 
  
   git stash
   if [ "$switch_branch" = "y" ] || [ "$switch_branch" = "Y" ]; then
 
     # ブランチ名をユーザーに入力してもらう
     echo "${branches}"
-    read -p "切り替えるブランチ名を入力してください:" branch_name
+    read -p "Enter the name of the branch:" branch_name
 
     # 既存のブランチに切り替える
     git checkout "$branch_name"
   else
     # checkout -b xxxx を実行する前に、ブランチ名をユーザーに入力してもらう
-    read -p "新しいブランチ名を入力してください: " new_branch_name
+    read -p "Enter the name of new branch: " new_branch_name
 
     # checkout -b xxxx を実行する
     git checkout -b "$new_branch_name"
@@ -41,7 +42,7 @@ fi
 git add --all
 
 # commit -m を実行する前に、コミットメッセージをユーザーに入力してもらう
-read -p "コミットメッセージを入力してください: " commit_message
+read -p "Enter the commit message: " commit_message
 
 # commit -m を実行する
 git commit -m "$commit_message"
