6f68556a1f0c726f13afcf1b4b74b007d8b4a92a
Harumagedon <g2227059@st.t-kougei.ac.jp>
2023-05-03 21:07:33 +0900

    update sample.cs

--- Sample.cs
+++ Sample.cs
@@ -1,11 +1,23 @@
 ﻿//ディレクトリ構造に準ずる(Scriptsは含まない)
 namespace Test.Sample
 {
+    /// <summary>
+    /// summaryはClass + public method
+    /// </summary>
     class Program : IInterface
     {
         //constであっても命名規則は変えない
         private const float tickRate = 10f;
         public int StartTime = 1;
+
+        private int aBC;
+        public event Action<int> SomeAction;
+
+        public int ABC
+        {
+            get => aBC;
+            set => aBC = value;
+        }
 
         private int pointA;
         
@@ -13,7 +25,11 @@ namespace Test.Sample
         //使う必要があればクラスを分けることを検討してみて
         //#region Setup Function
 
-        private void MethodA() { }
+
+        private void MethodA()
+        {
+            
+        }
 
         private int MethodB(int args)
         {
@@ -22,13 +38,21 @@ namespace Test.Sample
 
         private int MethodC(int pointA)
         {
-            //名前付き引数はつけてもつけなくても
-            //統一した方がいい？
-            MethodB(args: 3);
+            //名前付き引数はつけない
+            MethodB(3);
             
             //同義の場合はthisで自身のインスタンスを指して代入
             this.pointA = pointA;
+
+            SomeAction?.Invoke(1);
+
+            if (true)
+            {
+                return;
+            }
         }
+        
+        
     }
 }
 
