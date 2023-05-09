78e5b880460a1dd50f45a95e212cf450d471f060
Matuyuhi <bird9.yuhi@gmail.com>
2023-05-09 16:42:22 +0900

    config設定用ファイル

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# http.proxy
+git config --global http.proxy http://proxy-a.t-kougei.ac.jp:8080
+
+# https.proxy
+git config --global https.proxy http://proxy-a.t-kougei.ac.jp:8080
+
+# http.sslVerify
+git config --global http.sslVerify false
